bffd4446573492943862605697e9848aaf9351de
Diego Achury <achurycompany@gmail.com>
2013-03-25 10:52:07 -0500

    iniciando la aplicacion redcent

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redcent::Application.config.secret_token = '5d9ce89e435e25db1774b7175215ad9ff498faf4e83d133ac41091c5ceb139be4f878009a0ba5946c4a7a7ed2683c520c6e8836a930afd017e57f4d845af4d42'
